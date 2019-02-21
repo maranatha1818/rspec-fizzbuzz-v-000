95d74f88b98ce69bdd0762a80ee51acaecf96c09
Marja Houston <marjahouston@gmail.com>
2019-02-21 03:17:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz (int)
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 && 5 == 0
+  elsif int % 3 && 5
     "FizzBuzz"
-  elsif int !% 3 || 5 == 0
+  elsif int !% 3 || 5
     "nil"
   end
 end
